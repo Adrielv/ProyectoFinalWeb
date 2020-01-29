87785c91acaab7ac3e44a20a6c9e70cb74b26744
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2020-01-28 20:09:54 -0500

    Agregando varios detalless

---
+++ ProyectoFinalWeb/DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalWeb.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalWeb.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server = (local); Database = ProyectoFinalWebdb ; Trusted_Connection = True;");
+        }
+    }
+}
---
+++ ProyectoFinalWeb/Entidades/Clientes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalWeb.Entidades
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Ingrese un Nombree")]
+        public string Nombre { get; set; }
+
+
+        [Required(ErrorMessage = "Ingrese un Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Ingrese una dirrecion")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "Ingrese un telefono")]
+        public string Telefono { get; set; }
+
+
+
+
+        public Clientes()
+        {
+            ClienteId = 0;
+            Nombre = string.Empty;
+            Email = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+          
+        }
+    }
+}
